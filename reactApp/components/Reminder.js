ef59ae2c8e84b81ab1244c5cce2f40d73accb9ee
Amanda <ajhwhiz@gmail.com>
2017-08-13 09:32:22 -0700

    minor changes to reminders

--- reactApp/components/Reminder.js
+++ reactApp/components/Reminder.js
@@ -21,21 +21,21 @@ class Reminder extends React.Component {
         this.setState({toDo: resp.data});
       });
 
-    // // START SOCKETS STUFF:
-    // const self = this;
-    // this.state.socket.on('connect', () => {
-    //   // join socket as ToDo
-    //   console.log('CLIENT todo connected to sockets');
-    //   self.state.socket.emit('join', 'TODO');
-    //
-    //   // listen for end of stt
-    //   self.state.socket.on('stt_finished', respObj => {
-    //     console.log('received stt finished', respObj);
-    //     console.log('calling process request')
-    //     self.processRequest(respObj);
-    //   });
-    //
-    // });
+    // START SOCKETS STUFF:
+    const self = this;
+    this.state.socket.on('connect', () => {
+      // join socket as ToDo
+      console.log('CLIENT todo connected to sockets');
+      self.state.socket.emit('join', 'TODO');
+
+      // listen for end of stt
+      self.state.socket.on('stt_finished', respObj => {
+        console.log('received stt finished', respObj);
+        console.log('calling process request')
+        self.processRequest(respObj);
+      });
+
+    });
 
     // start listening at mount of component
     // this.startListening('TODO');
@@ -44,41 +44,41 @@ class Reminder extends React.Component {
 
   //function to add todo
   createToDo(task){
-    // console.log('in create todo task');
-    // //axios post request with given task
-    // axios.post('http://localhost:3000/todo', {task})
-    // //returns the Reminder object, push to this.state.toDo and update state
-    // .then( resp => {
-    //   let newToDo = [...this.state.toDo, {task: resp.data.task}]
-    //   console.log('creating new todo list', newToDo)
-    //   this.setState({toDo: newToDo})
-    // })
-    // .catch( err => {
-    //   console.log('ERROR: ', err);
-    //   // this.state.emit('invalid request')
-    // })
+    console.log('in create todo task');
+    //axios post request with given task
+    axios.post('http://localhost:3000/todo', {task})
+    //returns the Reminder object, push to this.state.toDo and update state
+    .then( resp => {
+      let newToDo = [...this.state.toDo, {task: resp.data.task}]
+      console.log('creating new todo list', newToDo)
+      this.setState({toDo: newToDo})
+    })
+    .catch( err => {
+      console.log('ERROR: ', err);
+      // this.state.emit('invalid request')
+    })
   };
 
   //funciton to delete todo
   deleteToDo(task){
-    // // post request to delete
-    // axios.post('http://localhost:3000/deltodo', {task})
-    // //return array of reminders in database without deleted reminder
-    // .then((resp) => {
-    //   this.setState({toDo: resp})
-    // })
-    // .catch( err => {
-    //   console.log('ERORRRR: ', err);
-    // });
+    // post request to delete
+    axios.post('http://localhost:3000/deltodo', {task})
+    //return array of reminders in database without deleted reminder
+    .then((resp) => {
+      this.setState({toDo: resp})
+    })
+    .catch( err => {
+      console.log('ERORRRR: ', err);
+    });
   };
 
   processRequest(respObj) {
     const self = this;
     console.log('in process request with ', respObj)
-   
+
     // command is under todo category --> process it
     if (respObj.category === 'todo') {
-    
+
       if (!respObj.params) {   // if no params, keep listening
         // self.startListening('TODO');
       } else if (!respObj.params.verb || !respObj.params.task) {   // keep listening if missing params
@@ -93,12 +93,12 @@ class Reminder extends React.Component {
       } else if (respObj.params.verb === 'delete') {   // command is to delete task
         self.deleteToDo(respObj.params.task);
       }
-    
+
       // need an else statement here ?
-    
+
       // command did not fall under todo --> ignore and start listening again
     } else {
-	  console.log('invalid request');
+	     console.log('invalid request');
       // self.state.socket.emit('invalid_request');
       // self.startListening('TODO');
     }
