d69d9943adc6bafc20a5c412da2f5e1bf1ae7f77
Yashvardhan Nevatia <yashvardhannevatia@Yashvardhans-MacBook-Pro.local>
2017-08-17 00:15:45 -0700

    uberrrrrr

--- reactApp/components/Uber.js
+++ reactApp/components/Uber.js
@@ -25,16 +25,14 @@ class Uber extends React.Component {
 
   componentDidMount() {
 
-    axios.put('http://localhost:3000/sandbox/status', {
-        request_id: 'ae6f6f3e-3f68-4c86-96b0-bd5f5910e90f',
-        status: 'driver_canceled',
-      }).then(resp => console.log('CANCELED', resp))
+    // axios.put('http://localhost:3000/sandbox/status', {
+    //     request_id: 'ae6f6f3e-3f68-4c86-96b0-bd5f5910e90f',
+    //     status: 'driver_canceled',
+    //   }).then(resp => console.log('CANCELED', resp))
 
-    // axios.get('http://localhost:3000/current1').then(resp => {
-    //   // console.log('DATA DATA DATA', resp)
-    //   this.setState({ request_id: resp.data.request_id })
-    //   console.log( this.state.request_id );
-    // });
+    axios.get('http://localhost:3000/current1').then(resp => {
+      console.log('CURRENT UBER REQUEST ID', resp.data.request_id);
+    });
 
     // GET AVAILABLE PRODUCTS
     axios.get('http://localhost:3000/products', {
@@ -150,7 +148,7 @@ class Uber extends React.Component {
     }).then((respDriverDetails) => {
       console.log('WE MADE IT TO THE END!! SUCCESS!!!', respDriverDetails)
       self.setState({ driverComing: true, driverDetails: respDriverDetails.data })
-      return self.sleep(120000);
+      return self.sleep(5000);
     })
     .then(() => {
       return self.clearUber();
