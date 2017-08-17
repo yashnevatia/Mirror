8f08781d45db72e151df9dd86a0c98e5ecdb5050
Yashvardhan Nevatia <yashvardhannevatia@Yashvardhans-MacBook-Pro.local>
2017-08-16 22:48:30 -0700

    uber clear

--- reactApp/components/Uber.js
+++ reactApp/components/Uber.js
@@ -25,10 +25,10 @@ class Uber extends React.Component {
 
   componentDidMount() {
 
-    axios.put('http://localhost:3000/sandbox/status', {
-        request_id: 'ae6f6f3e-3f68-4c86-96b0-bd5f5910e90f',
-        status: 'driver_canceled',
-      }).then(resp => console.log('CANCELED', resp))
+    // axios.put('http://localhost:3000/sandbox/status', {
+    //     request_id: 'ae6f6f3e-3f68-4c86-96b0-bd5f5910e90f',
+    //     status: 'driver_canceled',
+    //   }).then(resp => console.log('CANCELED', resp))
 
     // axios.get('http://localhost:3000/current1').then(resp => {
     //   // console.log('DATA DATA DATA', resp)
