173d6dcef71d52c65c1f5c938a0252a9ecd92ec4
asif521 <asifbhat521@gmail.com>
2017-08-17 17:15:06 -0700

    not working rn

--- reactApp/app.js
+++ reactApp/app.js
@@ -9,8 +9,8 @@ class Container extends React.Component {
   constructor() {
     super();
     this.state = {
-      isActive: true,
-	    widgets: ["reminders"],
+      isActive: false,
+	    widgets: [],
       initialResponses: {
         radio: 'Would you like to play, pause or search for songs?',
         news: 'Which news source would you like to view?',
